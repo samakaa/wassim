d73b47630765a871ee7928aef6bef01071b3c42e
wassim <dallellwassim@gmail.com>
2018-03-30 03:06:23 +0200

    modification simple

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :password_confirmation])
       devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar , :phone_number , :description, :email, :password, :password_confirmation])
   end
 end
