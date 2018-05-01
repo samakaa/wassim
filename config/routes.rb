377d33a6ba3255c9160e37f02643042370335987
wassim <dallellwassim@gmail.com>
2018-05-01 04:56:35 +0200

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users,
                :path=> '',
                :path_names=> {:sign_in=> 'login', :sign_out=> 'logout', :edit=> 'profile'},
-               :controllers=> {:registrations=> 'registrations'}
+               :controllers=> {:registrations=> 'registrations',
+               :confirmations => 'confirmations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pages#home'
 # chemin d'accès de la méthode show du controller User
