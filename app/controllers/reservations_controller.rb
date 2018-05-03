84cbbd23011658923cf109aede6a34971ec183f5
wassim <dallellwassim@gmail.com>
2018-05-03 03:35:19 +0200

    aaa

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,9 +24,18 @@ def preload
 def create
 
       @reservation = current_user.reservations.create(reservation_params)
+
+      if @reservation.save
+
+             AppMailer.new_reservation(Room.find(@reservation.room_id), @reservation).deliver_now
+
+             redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
+
+       
+
       redirect_to @reservation.room, notice:  "Votre réservation a été acceptée"
      end
-
+end
      def your_trips
 
             @trips = current_user.reservations
