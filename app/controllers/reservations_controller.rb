16491d852099cd284d0b4996492280e4684fe392
wassim <dallellwassim@gmail.com>
2018-05-22 05:14:29 +0200

    payment

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -35,8 +35,8 @@ def preload
 
         if @payment.save
             AppMailer.new_reservation(Room.find(@reservation.room_id), @reservation).deliver_now
-            redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
         end
+        redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
 
         rescue Exception
 
